8f87bebb56c881b5c075b7e387730ab6107992c9
Sanekim <sane2710@naver.com>
2019-03-13 17:52:49 +0900

    models.go 기본 페이지 추가

--- models/model.go
+++ models/model.go
@@ -1,1 +1,26 @@
 package models
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+)
+
+const (
+	// SQLConnectionString : MySQL Connection String
+	SQLConnectionString = "*"
+	// SALT : SALT
+	SALT = "*"
+)
+
+// Database Connection
+var db *gorm.DB
+
+func init() {
+	var err error
+	db, err = gorm.Open("mysql", SQLConnectionString)
+	if err != nil {
+		fmt.Println("db have error")
+	}
+}
+
